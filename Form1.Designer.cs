faa15c169a7900c251b52df6c4516b3e6d5308b5
kajal01 <30410491+kajal01@users.noreply.github.com>
2022-04-22 14:50:37 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace MatrixGridAnimationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Play = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.Pause = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.Resume = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.Stop = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AllowMerge = false;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Play,
+            this.toolStripSeparator1,
+            this.Pause,
+            this.toolStripSeparator3,
+            this.Resume,
+            this.toolStripSeparator2,
+            this.Stop,
+            this.toolStripSeparator4,
+            this.toolStripButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 53);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Play
+            // 
+            this.Play.AutoSize = false;
+            this.Play.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Play.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Play.Image = ((System.Drawing.Image)(resources.GetObject("Play.Image")));
+            this.Play.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(50, 50);
+            this.Play.Text = "Play";
+            this.Play.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.Play.Click += new System.EventHandler(this.OnClick_Play);
+            this.Play.DoubleClick += new System.EventHandler(this.OnClick_Play);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.AutoSize = false;
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(10, 53);
+            // 
+            // Pause
+            // 
+            this.Pause.AutoSize = false;
+            this.Pause.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Pause.Image = ((System.Drawing.Image)(resources.GetObject("Pause.Image")));
+            this.Pause.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(50, 50);
+            this.Pause.Text = "Pause";
+            this.Pause.Click += new System.EventHandler(this.OnClick_Pause);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 53);
+            // 
+            // Resume
+            // 
+            this.Resume.AutoSize = false;
+            this.Resume.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Resume.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Resume.Image = ((System.Drawing.Image)(resources.GetObject("Resume.Image")));
+            this.Resume.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Resume.Name = "Resume";
+            this.Resume.Size = new System.Drawing.Size(50, 50);
+            this.Resume.Text = "Resume";
+            this.Resume.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.Resume.Click += new System.EventHandler(this.Resume_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.toolStripSeparator2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 53);
+            // 
+            // Stop
+            // 
+            this.Stop.AutoSize = false;
+            this.Stop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Stop.Image = ((System.Drawing.Image)(resources.GetObject("Stop.Image")));
+            this.Stop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(50, 50);
+            this.Stop.Text = "Stop";
+            this.Stop.Click += new System.EventHandler(this.OnClick_Stop);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 53);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.AutoSize = false;
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(50, 50);
+            this.toolStripButton4.Text = "toolStripButton4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Matrix Grid";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Play;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton Pause;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton Stop;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton Resume;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MatrixGridAnimationApp
+{
+    public partial class Form1 : Form
+    {
+        private Thread workerThread = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            object number;
+            number = 0;          
+            
+        }
+
+        
+        private void IncrementCounter(object number)
+        {
+            int value = (int)number;
+            
+            while (true)
+            {
+                Draw(value);
+                Thread.Sleep(500);                
+                value++;
+                if (value == 9)
+                    value = 1;
+                Invalidate();
+            }
+        }        
+
+        private void OnClick_Play(object sender, EventArgs e)
+        {
+            ParameterizedThreadStart parameterizedThreadStart = new ParameterizedThreadStart(IncrementCounter);
+            workerThread = new Thread(parameterizedThreadStart);
+            workerThread.Start(1); //calls Draw() for generating 1 grid
+            
+        }
+
+        private void OnClick_Pause(object sender, EventArgs e)
+        {
+            workerThread.Suspend();
+        }
+
+        private void OnClick_Stop(object sender, EventArgs e)
+        {
+            workerThread.Abort();
+            workerThread.Join(500);
+            Invalidate();
+        }
+
+        private void Resume_Click(object sender, EventArgs e)
+        {
+            workerThread.Resume();
+        }
+
+
+        private void Draw(int value)
+        {
+            int size = value * value;
+            int[] x, y;
+            x = new int[size];
+            y = new int[size];
+            int a, b;
+            a = 50;
+            b = 70;            
+            Rectangle[] rectangles = new Rectangle[size];       
+            int k; // counter for rectangles
+            k = 0;
+            
+            Graphics graphics = CreateGraphics();
+            Pen myPen = new Pen(Color.Tomato);
+            myPen.Width = 2;
+
+            xCoordinate();
+            
+            yCoordinate();
+            
+            rectIndices(x, y);
+            workerThread.Join(500); //if not used, misses some grid.
+            graphics.DrawRectangles(myPen, rectangles);
+            graphics.Dispose();
+            
+        
+            void rectIndices(int[] sample1, int[] sample2)
+            {
+                for (int i = 0; i < value; i++)  //for x coordinate
+                {
+                    for (int j = 0; j < value; j++)  //for y coordinate
+                    {   
+                        
+                        rectangles[k] = new Rectangle(sample1[i], sample2[j], 20, 20);
+                        k++;
+                    }
+                }
+            }
+
+            void xCoordinate()
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    x[i] = a;
+                    a += 20;
+                }
+            }
+
+            void yCoordinate()
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    y[i] = b;
+                    b += 20;
+                }
+            }
+        }
+
+       
+    }       
+}
